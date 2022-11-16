5cb384f8256bd18599a4c36e0a792704f3b4f231
lucas3k <lcamargostivan@gmail.com>
2022-11-16 18:06:43 -0300

    ajust page dash

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,21 @@
 import "./DashBoardStyle.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import moment from "moment";
 import { Header } from "../../shared/components/header/Header";
 import { HelmetTitle } from "../../shared/components/HelmetTitle";
 import { api } from "../../shared/services/api/api";
-import { Inputs } from "../../shared/components/inputs/Inputs";
 import { Buttons } from "../../shared/components/buttons/Buttons";
 import { ModalBase } from "./components/ModalBase";
 import { ModalDados } from "./components/ModalDados";
@@ -19,12 +28,43 @@ const Dashboard = () => {
   const [description, setDescription] = useState("");
   const [valueDate, setValueDate] = useState(moment());
 
+  const [allTransaction, setAllTransaction] = useState([]);
+  const [token, setToken] = useState("");
+
+  const [fullRevenues, setFullRevenues] = useState(0);
+  const [fullExpenses, setFullExpenses] = useState(0);
+  const [fullBalance, setFullBalance] = useState(0);
+
   const [openModalNewItem, setOpenModalNewItem] = useState(false);
   const toggleOpenedModalNewItem = () => setOpenModalNewItem(!openModalNewItem);
 
-  const token = localStorage.getItem("token");
+  const queryGetAllTransaction = () => {
+    if (!token) return;
+
+    api
+      .get("/transaction", { headers: { "x-access-token": token } })
+      .then((response) => setAllTransaction(response.data))
+      .catch((error) => {
+        Notification("error", error.response.data.error.message);
+      });
+  };
+
+  useEffect(() => {
+    const isToken = localStorage.getItem("token");
+    if (isToken) setToken(isToken);
+  }, []);
+
+  useEffect(() => {
+    queryGetAllTransaction();
+  }, [token]);
+
+  useEffect(() => {
+    const value = allTransaction.reduce((acc, item) => (acc += item.value), 0);
+    setFullRevenues(value);
+  }, [allTransaction]);
 
   const handleClickLogout = () => {
+    setToken("");
     localStorage.removeItem("token");
 
     api
@@ -45,17 +85,18 @@ const Dashboard = () => {
         { headers: { "x-access-token": token } }
       )
       .then((response) => {
-        console.log(response);
+        Notification("info", response.data.message);
       })
-      .catch((error) => {
-        Notification("error", error.response.data.error.message);
-      });
+      .catch((error) =>
+        Notification("error", error.response.data.error.message)
+      )
+      .finally(() => queryGetAllTransaction());
   };
 
   const getDados = () => {
     const real = Number(valueReal.trim());
     const desc = description.trim();
-    const data = moment(valueDate).format("DD/MM/YYYY").trim();
+    const data = new Date(valueDate).toISOString();
 
     if (real === "" || desc === "" || data === "") {
       Notification("info", "Preencha todos os campos!");
@@ -69,6 +110,13 @@ const Dashboard = () => {
     }
   };
 
+  const formatNumberToBR = (num = 0) => {
+    return Number(num).toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  };
+
   return (
     <>
       <HelmetTitle title="Dashboard" />
@@ -78,19 +126,73 @@ const Dashboard = () => {
       <Box component="section" className="containerDash">
         <Box component="section" className="containerInputsDash">
           <Box className="containerModalBtn">
-            <Inputs label="Receita" disabled={true} />
+            <Typography component="p">
+              {!!fullRevenues
+                ? `Receitas ${formatNumberToBR(fullRevenues)}`
+                : "Receitas - sem valor"}
+            </Typography>
+
             <Buttons text="+" handleClick={toggleOpenedModalNewItem} />
           </Box>
 
           <Box className="containerModalBtn">
-            <Inputs label="Despesas" disabled={true} />
+            <Typography component="p">
+              {!!fullExpenses
+                ? `Despesas ${formatNumberToBR(fullExpenses)}`
+                : "Despesas - sem valor"}
+            </Typography>
+
             <Buttons text="+" />
           </Box>
 
-          <Inputs label="Saldo" disabled={true} />
+          <Box>
+            <Typography component="p">
+              {`Valor total: ${formatNumberToBR(fullRevenues - fullExpenses)}`}
+            </Typography>
+          </Box>
         </Box>
 
-        <h1>Dashboard</h1>
+        {!!allTransaction.length && (
+          <TableContainer component="section" sx={{ mt: 4 }}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Tipo</TableCell>
+                  <TableCell align="right">Valor</TableCell>
+                  <TableCell align="right">Descrição</TableCell>
+                  <TableCell align="right">Data</TableCell>
+                  <TableCell align="right">Ações</TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {allTransaction
+                  .map((row) => (
+                    <TableRow
+                      key={row.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.typeId}
+                      </TableCell>
+                      <TableCell align="right">
+                        {formatNumberToBR(row.value)}
+                      </TableCell>
+                      <TableCell align="right">{row.description}</TableCell>
+                      <TableCell align="right">
+                        {moment(row.date).format("DD/MM/YYYY")}
+                      </TableCell>
+                      <TableCell align="right">
+                        <Button>Editar</Button>
+                        <Button>Excluir</Button>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                  .reverse()}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
       </Box>
 
       <ModalBase
